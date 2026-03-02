62a5478df837e6fcf45c128794209bef221bdb97
palawanpay-master <master@palawanpay.com>
2026-03-02 10:23:36 +0800

    Update AWS configuration with real resource IDs

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -1,15 +1,15 @@
 const config = {
   Auth: {
     Cognito: {
-      userPoolId: 'ap-southeast-1_XXXXXXXXX',
-      userPoolClientId: 'XXXXXXXXXXXXXXXXXXXXXXXXXX',
-      identityPoolId: 'ap-southeast-1:XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX',
+      userPoolId: 'ap-southeast-1_YdUsCa38i',
+      userPoolClientId: '67sai9k433mpqs87f34im74prt',
+      identityPoolId: 'ap-southeast-1:6728d3e2-e8bc-416b-9d9d-c8d7a1028881',
       loginWith: {
         oauth: {
           domain: 'palawanpay-s3browser.auth.ap-southeast-1.amazoncognito.com',
           scopes: ['openid', 'email', 'profile'],
-          redirectSignIn: ['http://localhost:3000/', 'https://main.XXXXXX.amplifyapp.com/'],
-          redirectSignOut: ['http://localhost:3000/', 'https://main.XXXXXX.amplifyapp.com/'],
+          redirectSignIn: ['http://localhost:3000/', 'https://main.d3v8example.amplifyapp.com/'],
+          redirectSignOut: ['http://localhost:3000/', 'https://main.d3v8example.amplifyapp.com/'],
           responseType: 'code'
         }
       }
@@ -18,7 +18,7 @@ const config = {
   API: {
     REST: {
       S3BrowserAPI: {
-        endpoint: 'https://XXXXXXXXXX.execute-api.ap-southeast-1.amazonaws.com/prod',
+        endpoint: 'https://c9bc3h6l94.execute-api.ap-southeast-1.amazonaws.com/prod',
         region: 'ap-southeast-1'
       }
     }
