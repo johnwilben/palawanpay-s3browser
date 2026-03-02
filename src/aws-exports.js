c902e72f35a7e1c7c00ee9ee0388c7d304738ca2
palawanpay-master <master@palawanpay.com>
2026-03-02 10:59:31 +0800

    Switch to SPA client without secret

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -2,14 +2,14 @@ const config = {
   Auth: {
     Cognito: {
       userPoolId: 'ap-southeast-1_YdUsCa38i',
-      userPoolClientId: '67sai9k433mpqs87f34im74prt',
+      userPoolClientId: '19lg9i63etaa6322ml0jv0clqu',
       identityPoolId: 'ap-southeast-1:6728d3e2-e8bc-416b-9d9d-c8d7a1028881',
       loginWith: {
         oauth: {
           domain: 'palawanpay-s3browser.auth.ap-southeast-1.amazoncognito.com',
-          scopes: ['openid', 'email', 'profile'],
-          redirectSignIn: ['http://localhost:3000/', 'https://main.drm7arslkowgf.amplifyapp.com/'],
-          redirectSignOut: ['http://localhost:3000/', 'https://main.drm7arslkowgf.amplifyapp.com/'],
+          scopes: ['openid', 'email', 'profile', 'aws.cognito.signin.user.admin'],
+          redirectSignIn: ['https://main.drm7arslkowgf.amplifyapp.com/', 'http://localhost:3000/'],
+          redirectSignOut: ['https://main.drm7arslkowgf.amplifyapp.com/', 'http://localhost:3000/'],
           responseType: 'code'
         }
       }
