351a27a625c19b26fb81ffa0149f737e9eed23c6
palawanpay-master <master@palawanpay.com>
2026-03-04 16:43:00 +0800

    Use separate Cognito for account 721010870103

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -1,28 +1,21 @@
-const config = {
-  Auth: {
-    Cognito: {
-      userPoolId: 'ap-southeast-1_YdUsCa38i',
-      userPoolClientId: '19lg9i63etaa6322ml0jv0clqu',
-      identityPoolId: 'ap-southeast-1:6728d3e2-e8bc-416b-9d9d-c8d7a1028881',
-      loginWith: {
-        oauth: {
-          domain: 'palawanpay-s3browser.auth.ap-southeast-1.amazoncognito.com',
-          scopes: ['openid', 'email', 'profile', 'aws.cognito.signin.user.admin'],
-          redirectSignIn: ['https://feature-cross-account-access.drm7arslkowgf.amplifyapp.com/', 'https://main.drm7arslkowgf.amplifyapp.com/', 'http://localhost:3000/'],
-          redirectSignOut: ['https://feature-cross-account-access.drm7arslkowgf.amplifyapp.com/', 'https://main.drm7arslkowgf.amplifyapp.com/', 'http://localhost:3000/'],
-          responseType: 'code'
-        }
-      }
-    }
-  },
-  API: {
-    REST: {
-      S3BrowserAPI: {
-        endpoint: 'https://c9bc3h6l94.execute-api.ap-southeast-1.amazonaws.com/prod',
-        region: 'ap-southeast-1'
-      }
-    }
+const awsconfig = {
+  aws_project_region: 'ap-southeast-1',
+  aws_cognito_region: 'ap-southeast-1',
+  aws_user_pools_id: 'ap-southeast-1_ieR5X01hf',
+  aws_user_pools_web_client_id: '504f5m01uepjpq2d0v4i8ceh15',
+  oauth: {
+    domain: 's3browser-721010870103.auth.ap-southeast-1.amazoncognito.com',
+    scope: ['openid', 'email', 'profile'],
+    redirectSignIn: [
+      'https://feature-cross-account-access.d32el4qcx14shm.amplifyapp.com/',
+      'http://localhost:3000/'
+    ],
+    redirectSignOut: [
+      'https://feature-cross-account-access.d32el4qcx14shm.amplifyapp.com/',
+      'http://localhost:3000/'
+    ],
+    responseType: 'code'
   }
 };
 
-export default config;
+export default awsconfig;
