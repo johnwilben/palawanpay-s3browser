0282f70c6aa98df0ed88cf6f6f0df6ce782d3877
palawanpay-master <master@palawanpay.com>
2026-03-05 11:39:38 +0800

    Fix API configuration format for Amplify v6

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -1,22 +1,26 @@
 const awsconfig = {
-  aws_project_region: 'ap-southeast-1',
-  aws_cognito_region: 'ap-southeast-1',
-  aws_user_pools_id: 'ap-southeast-1_ieR5X01hf',
-  aws_user_pools_web_client_id: '504f5m01uepjpq2d0v4i8ceh15',
-  oauth: {
-    domain: 's3browser-721010870103.auth.ap-southeast-1.amazoncognito.com',
-    scope: ['openid', 'email', 'profile'],
-    redirectSignIn: 'https://feature-cross-account-access.d32el4qcx14shm.amplifyapp.com/,http://localhost:3000/',
-    redirectSignOut: 'https://feature-cross-account-access.d32el4qcx14shm.amplifyapp.com/,http://localhost:3000/',
-    responseType: 'code'
+  Auth: {
+    Cognito: {
+      userPoolId: 'ap-southeast-1_ieR5X01hf',
+      userPoolClientId: '504f5m01uepjpq2d0v4i8ceh15',
+      loginWith: {
+        oauth: {
+          domain: 's3browser-721010870103.auth.ap-southeast-1.amazoncognito.com',
+          scopes: ['openid', 'email', 'profile'],
+          redirectSignIn: ['https://feature-cross-account-access.d32el4qcx14shm.amplifyapp.com/', 'http://localhost:3000/'],
+          redirectSignOut: ['https://feature-cross-account-access.d32el4qcx14shm.amplifyapp.com/', 'http://localhost:3000/'],
+          responseType: 'code'
+        }
+      }
+    }
   },
   API: {
-    endpoints: [
-      {
-        name: 'S3BrowserAPI',
-        endpoint: 'https://9th34ei7t8.execute-api.ap-southeast-1.amazonaws.com/prod'
+    REST: {
+      S3BrowserAPI: {
+        endpoint: 'https://9th34ei7t8.execute-api.ap-southeast-1.amazonaws.com/prod',
+        region: 'ap-southeast-1'
       }
-    ]
+    }
   }
 };
 
